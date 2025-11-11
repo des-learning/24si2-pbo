933b85cd02e9ad01fff5c68c46ddea8c79a67bfb
Des Dulianto <desdulianto@gmail.com>
2025-11-11 18:43:17 +0700

    pertemuan 12: catch multiple exception

--- pertemuan12/ContohException.java
+++ pertemuan12/ContohException.java
@@ -5,16 +5,26 @@ public class ContohException {
         Scanner input = new Scanner(System.in);
 
         int a, b;
-        System.out.print("Input a: ");
-        a = input.nextInt();
-
         try {
+            System.out.print("Input a: ");
+            a = input.nextInt();
+
             System.out.print("Input b: ");
             b = input.nextInt();
 
             System.out.println(a + "/" + b + "=" + (a/b));
+        } catch (java.util.InputMismatchException e) {
+            System.err.println("Nilai yang diinput harus berupa angka");
+        } catch (java.lang.ArithmeticException e) {
+            System.err.println("Kesalahan melakukan operasi aritmatika: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("Nilai yang diinput salah");
+            if (e.getMessage() != null) {
+                System.err.println("Terjadi kesalahan: " + e.getMessage());
+            } else {
+                System.err.println("Terjadi Kesalahan: " + e.getClass().getName());
+            }
+        } finally {
+            System.out.println("Program selesai");
         }
     }
 }
